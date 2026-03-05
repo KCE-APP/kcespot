75e890e1737e8c6047f9607040d727d21a6b451f
Poovarasan <poovarasan936161@gmail.com>
2026-03-05 13:26:29 +0530

    fix:server

--- server.js
+++ server.js
@@ -37,11 +37,14 @@ const corsOptions = {
     "Authorization",
     "x-app-client",
     "ngrok-skip-browser-warning",
+    "Accept",
   ],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
-app.options(/.*/, cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
